7abefc2ed6c71e4e6a55afdc559b8cabc7f990d8
Eduardo Secolin <dusecolin@yahoo.com.br>
2018-10-22 09:49:39 -0300

    Lista de eventos

--- src/main/java/com/teste/eventoapp/controller/EventoController.java
+++ src/main/java/com/teste/eventoapp/controller/EventoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class EventoController {
@@ -22,5 +23,13 @@ public class EventoController {
     public String form(Evento evento){
         eventoRepository.save(evento);
         return "redirect:/registerController";
+    }
+
+    @RequestMapping("/eventos")
+    public ModelAndView listEvents(){
+        ModelAndView mv = new ModelAndView("index");
+        Iterable<Evento> eventos = eventoRepository.findAll();
+        mv.addObject("eventoapp", eventos);
+        return mv;
     }
 }
